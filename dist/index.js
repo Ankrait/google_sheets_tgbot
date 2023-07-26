22f767dd7b7082932e035120d7f6157ad7a110ed
Азат Файзрахманов <azat_fayzrahmanov@mail.ru>
2023-07-26 17:14:17 +0300

    changes

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+app_1.bot.launch().then(() => {
+    console.log('Бот запущен');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { bot } from './src/app';
+import { bot } from './app';
 
 bot.launch().then(() => {
 	console.log('Бот запущен');
